604210a338443a53da661cac6be2564d72e94640
123 <55683566+wannabetter@users.noreply.github.com>
2023-05-10 13:29:11 +0800

    Add files via upload

---
+++ 1015.py
@@ -0,0 +1,7 @@
+def smallestRepunitDivByK(k: int) -> int:
+    seen = set()
+    x = 1 % k
+    while x and x not in seen:
+        seen.add(x)
+        x = (x * 10 + 1) % k
+    return -1 if x else len(seen) + 1
