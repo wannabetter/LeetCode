931fd93e9cceb356febc1471a3271081bd15c250
123 <55683566+wannabetter@users.noreply.github.com>
2023-05-23 13:54:05 +0800

    Add files via upload

---
+++ 1090.py
@@ -0,0 +1,23 @@
+import heapq
+from typing import List
+from collections import Counter
+
+
+def largestValsFromLabels(values: List[int], labels: List[int], numWanted: int, useLimit: int) -> int:
+    n = len(values)
+    idx = list(range(n))
+    idx.sort(key=lambda i: -values[i])
+
+    ans = choose = 0
+    cnt = Counter()
+    for i in range(n):
+        label = labels[idx[i]]
+        if cnt[label] == useLimit:
+            continue
+
+        choose += 1
+        ans += values[idx[i]]
+        cnt[label] += 1
+        if choose == numWanted:
+            break
+    return ans
