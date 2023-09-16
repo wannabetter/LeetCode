8a205a2b11f3c9f6fd48bb2b7ad1cf9b29ac9f97
123 <55683566+wannabetter@users.noreply.github.com>
2023-09-16 12:57:17 +0800

    Add files via upload

--- 198.py
+++ 198.py
@@ -2,10 +2,9 @@ from typing import List
 
 
 def rob(nums: List[int]) -> int:
-    if len(nums) == 1:
-        return nums[0]
-    dp = [0 for _ in range(0, len(nums))]
-    dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-    for i in range(2, len(nums)):
-        dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+    if len(nums) <= 2:
+        return max(nums)
+    dp = [nums[0], max(nums[0], nums[1])]
+    for index in range(2, len(nums)):
+        dp.append(max(dp[-1], dp[-2] + nums[index]))
     return dp[-1]
