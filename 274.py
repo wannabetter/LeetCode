e8dfda039109d92491071f8ff027c23d2aa3e636
123 <55683566+wannabetter@users.noreply.github.com>
2023-10-29 00:58:14 +0800

    Add files via upload

---
+++ 274.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def hIndex(citations: List[int]) -> int:
+    citations.sort(reverse=True)
+    left, right = 0, len(citations) - 1
+    while left < right:
+        mid = (left + right + 1) // 2
+        if citations[mid] >= mid + 1:
+            left = mid
+        else:
+            right = mid - 1
+    if citations[left] == 0:
+        return 0
+    return left + 1
