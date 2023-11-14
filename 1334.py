b45edb7d65ee399ddd0bf1cdb0d9ab87ac10380a
123 <55683566+wannabetter@users.noreply.github.com>
2023-11-14 13:39:07 +0800

    Add files via upload

---
+++ 1334.py
@@ -0,0 +1,20 @@
+from typing import List
+from math import inf
+
+
+def findTheCity(n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+    ans = (inf, -1)
+    mp = [[inf] * n for _ in range(n)]
+
+    for fr, to, weight in edges:
+        mp[fr][to], mp[to][fr] = weight, weight
+    for k in range(n):
+        mp[k][k] = 0
+        for i in range(n):
+            for j in range(n):
+                mp[i][j] = min(mp[i][j], mp[i][k] + mp[k][j])
+    for i in range(n):
+        cnt = sum(mp[i][j] <= distanceThreshold for j in range(n))
+        if cnt <= ans[0]:
+            ans = (cnt, i)
+    return ans[1]
