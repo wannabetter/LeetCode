8ddcd98c0ec38f0cd4ae32687af07545eadd7909
123 <55683566+wannabetter@users.noreply.github.com>
2024-01-15 14:16:23 +0800

    Add files via upload

--- 82.py
+++ 82.py
@@ -1,20 +1,26 @@
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-def deleteDuplicates(head: ListNode) -> ListNode:
-    if not head:
-        return head
-    else:
-        Dummy = ListNode(0, head)
-        Cur = Dummy
-        while Cur.next and Cur.next.next:
-            if Cur.next.val == Cur.next.next.val:
-                Val = Cur.next.val
-                while Cur.next and Cur.next.val == Val:
-                    Cur.next = Cur.next.next
-            else:
-                Cur = Cur.next
-        return Dummy.next
+def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(val=-1, next=head)
+    prev, cur = dummy, dummy.next
+    while cur and cur.next:
+        if cur.val == cur.next.val:
+            val = cur.val
+            while cur and val == cur.val:
+                cur = cur.next
+            prev.next = cur
+        else:
+            cur = cur.next
+            prev = prev.next
+    return dummy.next
+
+
+
+
