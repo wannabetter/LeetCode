4cad71c4356a23e5ffc63a3116cfff4b75283a04
123 <55683566+wannabetter@users.noreply.github.com>
2024-01-31 20:13:45 +0800

    Add files via upload

---
+++ 2670.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def distinctDifferenceArray(nums: List[int]) -> List[int]:
+    n = len(nums)
+    ans = [0] * n
+
+    for index in range(n):
+        ans[index] = len(set(nums[:index + 1])) - len(set(nums[index + 1:]))
+
+    return ans
