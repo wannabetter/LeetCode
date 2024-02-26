d07e3dacd89ad1fbda7b3bb9320182ba038ba740
123 <55683566+wannabetter@users.noreply.github.com>
2024-02-26 13:41:31 +0800

    Add files via upload

---
+++ 938.cpp
@@ -0,0 +1,35 @@
+//
+// Created by DELL on 2024/2/26.
+//
+
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode *root, int low, int high) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->val < low) {
+            return rangeSumBST(root->right, low, high);
+        }
+        if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        }
+        return root->val + rangeSumBST(root->right, low, high) + rangeSumBST(root->left, low, high);
+    }
+};
